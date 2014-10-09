01d510ba5bb44edd7fddd1286eec1c23a121b733
Skovy <spencer@skovistudios.com>
2014-10-09 14:39:02 -0400

    added text field to survey, began mysql setup

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 ## A Generic PHP Survey
 
+#### MySQL Query to Create Database & Table
+* run first time only:
+```mysql
+CREATE DATABASE survey;
+USE survey;
+```
+* create tables (change the table prefix for a new survey)
+```mysql
+SET @PREFIX = "s1"; # change prefix
+
+SET @QUERY = CONCAT('
+CREATE TABLE IF NOT EXISTS ', @PREFIX, '_questions (
+  id int(11) NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY(id)
+)');
+
+PREPARE stmt FROM @QUERY;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
+```
--- system/application/views/templates/survey/survey.php
+++ system/application/views/templates/survey/survey.php
@@ -1,12 +1,24 @@
   <div class="container">
     <div class="row">
       <div class="col-md-12">
+        <h2 class="text-center">
+          Survey Title
+          <small class="show">
+            Quick Surevy Blurb
+          </small>
+        </h2>
         <form role="form">
           <div class="form-group">
             <label for="email_field">
-              Email Address
+              Email Address <small>Required</small>
             </label>
             <input type="email" class="form-control" id="email_field" placeholder="Enter Email Address">
+          </div>
+          <div class="form-group">
+            <label for="text_field">
+              Text Information
+            </label>
+            <textarea class="form-control" rows="3" id="text_field" placeholder="Enter Response"></textarea>
           </div>
           <button type="submit" class="btn btn-success pull-right">Submit</button>
         </form>
